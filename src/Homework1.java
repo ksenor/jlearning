14ea41c213676ed20738b4fd201f2f778dccfb11
ks <ks@ks-work.(none)>
2013-04-01 14:44:48 +0400

    ok

---
+++ src/Homework1.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ks
+ * Date: 4/1/13
+ * Time: 2:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Homework1 {
+
+    public static void main(String args[]) {
+        int i;
+        for (i=0; i<=100; i++) {
+            if ((i%3==0)&&(i%5!=0))
+                System.out.println(i + ": Mama");
+
+
+            if ((i%3!=0)&&(i%5==0)) {
+                System.out.println(i + ": Papa");
+            }
+            if ((i%3==0)&&(i%5==0)) {
+                System.out.println(i + ": PapaMama");
+            }
+        }
+    }
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ks
+ * Date: 3/26/13
+ * Time: 2:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    static String main = null;
+
+    public static void main(String[] args) {
+        System.out.println(main.format("hello %s", "Vasja"));
+                              main.hashCode();
+       // main = new Main();
+        System.out.println(main.toString());
+    }
+}
